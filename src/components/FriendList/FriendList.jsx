115ba91e2fd88525929917a269fcc6fb5fab7412
Vitaliy Ivanov <dotaivanov@gmail.com>
2022-10-09 13:05:20 +0300

    fixed list

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,9 +1,28 @@
+import PropTypes from 'prop-types';
 import { List } from './FriendList.styled';
 import { FriendsListItem } from './FriendListItem';
+
 export const FriendList = ({ friends }) => {
   return (
     <List>
-      <FriendsListItem friends={friends} />
+      {friends.map(friend => (
+        <FriendsListItem
+          key={friend.id}
+          name={friend.name}
+          avatar={friend.avatar}
+          isOnline={friend.isOnline}
+        />
+      ))}
     </List>
   );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.array,
+  friend: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string,
+    avatar: PropTypes.string,
+    isOnline: PropTypes.bool,
+  }),
 };
--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,13 +1,19 @@
+import PropTypes from 'prop-types';
+
 import { ListItem, StatusIndicator } from './FriendList.styled';
 
-export const FriendsListItem = ({ friends }) => {
-  return friends.map(({ id, name, avatar, isOnline }) => {
-    return (
-      <ListItem key={id}>
-        <StatusIndicator isOnline={isOnline}></StatusIndicator>
-        <img src={avatar} alt={name} width="48" />
-        <p>{name}</p>
-      </ListItem>
-    );
-  });
+export const FriendsListItem = ({ isOnline, avatar, name }) => {
+  return (
+    <ListItem>
+      <StatusIndicator isOnline={isOnline}></StatusIndicator>
+      <img src={avatar} alt={name} width="48" />
+      <p>{name}</p>
+    </ListItem>
+  );
+};
+
+FriendsListItem.propTypes = {
+  isOnline: PropTypes.bool,
+  avatar: PropTypes.string,
+  name: PropTypes.string,
 };
